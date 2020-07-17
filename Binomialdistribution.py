70b9b2b46d9ef9735df34d25a6f97e004595f8d7
Billy Hansen <bhansen@nsareit.net>
2020-07-17 15:27:21 -0700

    Round standard deviation in represent function

--- Binomialdistribution.py
+++ Binomialdistribution.py
@@ -163,4 +163,4 @@ class Binomial(Distribution):
 
         """
 
-        return "Mean: {}, Standard Deviation: {}, p: {}, n: {}.".format(self.mean, self.stdev, self.p, self.n)
+        return "Mean: {}, Standard Deviation: {}, p: {}, n: {}.".format(self.mean, round(self.stdev), self.p, self.n)
